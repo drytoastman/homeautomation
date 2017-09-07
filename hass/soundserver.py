1dbe583c6cc52a4f4225b04b5ec7c476a29ffc3f
Brett Wilson <drytoastman@gmail.com>
2017-09-06 20:56:14 -0700

    bring the door bell pieces

--- hass/soundserver.py
+++ hass/soundserver.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python3
 
-import time
+import glob
 import os
+import time
 import yaml
 
 import paho.mqtt.client as mqtt
 from AppKit import NSSound
 
+sounds = dict()
+basedir = os.path.dirname(__file__)
+
+def load_sound(name, loop=False):
+    f = os.path.join(basedir, 'sounds', name+'.wav')
+    sounds[name] = NSSound.alloc()
+    sounds[name].initWithContentsOfFile_byReference_(f, True)
+    if loop:
+        sounds[name].setLoops_(True)
+
 def on_connect(client, userdata, flags, rc):
     client.subscribe("switches/set/#")
 
 def on_message(client, userdata, msg):
-    if msg.topic == 'switches/set/warning':
+    if msg.topic.startswith('switches/set/'):
+        sound = msg.topic[13:]
+        if sound not in sounds:
+            return
         if msg.payload == b'ON':
-            warning.play()
-            client.publish('switches/pub/warning', 'ON')
+            sounds[sound].play()
+            client.publish('switches/pub/'+sound, 'ON')
         else:
-            warning.stop()
-            client.publish('switches/pub/warning', 'OFF')
+            sounds[sound].stop()
+            client.publish('switches/pub/'+sound, 'OFF')
 
 # Find out where the config files are and load our HASS api password
-basedir = os.path.dirname(__file__)
-with open(os.path.join(basedir, 'http.yaml'), 'r') as fp:
+with open(os.path.join(basedir, 'secrets.yaml'), 'r') as fp:
     config = yaml.load(fp)
-    pw = config['api_password']
+    pw = config['http_password']
 
 # Now load any sounds we want
-warning = NSSound.alloc()
-warning.initWithContentsOfFile_byReference_(os.path.join(basedir, 'sounds', 'warning.wav'), True)
-warning.setLoops_(True)
+load_sound('warning', True)
+load_sound('doorbell', False)
 
 # And start of the MQTT connection through the docker-machine
 client = mqtt.Client("sound-client")
